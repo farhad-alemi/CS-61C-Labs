961a41be0c007fbc3e378fedcb2e1089894314fb
Farhad Alemi <farhad.alemi@berkeley.edu>
2020-11-28 08:58:21 -0800

    added simd.c

--- lab09/simd.c
+++ lab09/simd.c
@@ -48,14 +48,34 @@ long long int sum_unrolled(int vals[NUM_ELEMS]) {
 long long int sum_simd(int vals[NUM_ELEMS]) {
 	clock_t start = clock();
 	__m128i _127 = _mm_set1_epi32(127);		// This is a vector with 127s in it... Why might you need this?
-	long long int result = 0;				   // This is where you should put your final result!
-	/* DO NOT DO NOT DO NOT DO NOT WRITE ANYTHING ABOVE THIS LINE. */
-	
+	long long int result = 0;				    // This is where you should put your final result!
+	/* DO NOT WRITE ANYTHING ABOVE THIS LINE. */
+
 	for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
-		/* YOUR CODE GOES HERE */
+	    int temp_arr[4];
+        __m128i sum = _mm_setzero_si128();      // returns a 128-bit zero vector
 
-		/* You'll need a tail case. */
+        for (unsigned int i = 0; i < NUM_ELEMS / 4 * 4; i+=4) {
+            __m128i temp_vals, mask;
 
+            temp_vals = _mm_loadu_si128((const __m128i_u *) &(vals[i]));
+            mask = _mm_cmpgt_epi32(temp_vals, _127);
+            temp_vals = _mm_and_si128(temp_vals, mask);
+
+            sum = _mm_add_epi32(sum, temp_vals);
+        }
+        _mm_storeu_si128((__m128i_u *) temp_arr, sum);
+
+        for(int i = 0; i < 4; ++i) {
+            result += temp_arr[i];
+        }
+
+        /* Tail Case */
+        for(unsigned int i = NUM_ELEMS / 4 * 4; i < NUM_ELEMS; i++) {
+            if (vals[i] >= 128) {
+                result += vals[i];
+            }
+        }
 	}
 	clock_t end = clock();
 	printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
@@ -67,11 +87,52 @@ long long int sum_simd_unrolled(int vals[NUM_ELEMS]) {
 	__m128i _127 = _mm_set1_epi32(127);
 	long long int result = 0;
 	for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
-		/* COPY AND PASTE YOUR sum_simd() HERE */
-		/* MODIFY IT BY UNROLLING IT */
+        int temp_arr[4];
+        __m128i sum = _mm_setzero_si128();      // returns a 128-bit zero vector
 
-		/* You'll need 1 or maybe 2 tail cases here. */
+        for (unsigned int i = 0; i < NUM_ELEMS / 16 * 16; i+=16) {
+            __m128i temp_vals, mask;
 
+            /* First Iteration */
+            temp_vals = _mm_loadu_si128((const __m128i_u *) &(vals[i]));
+            mask = _mm_cmpgt_epi32(temp_vals, _127);
+            temp_vals = _mm_and_si128(temp_vals, mask);
+
+            sum = _mm_add_epi32(sum, temp_vals);
+
+            /* Second Iteration */
+            temp_vals = _mm_loadu_si128((const __m128i_u *) &(vals[i + 4]));
+            mask = _mm_cmpgt_epi32(temp_vals, _127);
+            temp_vals = _mm_and_si128(temp_vals, mask);
+
+            sum = _mm_add_epi32(sum, temp_vals);
+
+            /* Third Iteration */
+            temp_vals = _mm_loadu_si128((const __m128i_u *) &(vals[i + 8]));
+            mask = _mm_cmpgt_epi32(temp_vals, _127);
+            temp_vals = _mm_and_si128(temp_vals, mask);
+
+            sum = _mm_add_epi32(sum, temp_vals);
+
+            /* Fourth Iteration */
+            temp_vals = _mm_loadu_si128((const __m128i_u *) &(vals[i + 12]));
+            mask = _mm_cmpgt_epi32(temp_vals, _127);
+            temp_vals = _mm_and_si128(temp_vals, mask);
+
+            sum = _mm_add_epi32(sum, temp_vals);
+        }
+        _mm_storeu_si128((__m128i_u *) temp_arr, sum);
+
+        for (int i = 0; i < 4; ++i) {
+            result += temp_arr[i];
+        }
+
+        /* Tail Case */
+        for (unsigned int i = NUM_ELEMS / 16 * 16; i < NUM_ELEMS; i++) {
+            if (vals[i] >= 128) {
+                result += vals[i];
+            }
+        }
 	}
 	clock_t end = clock();
 	printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
---
+++ lab10/cmake-build-debug/CMakeFiles/lab10.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab10.dir/dotp.c.obj"
+  "CMakeFiles/lab10.dir/hello.c.obj"
+  "CMakeFiles/lab10.dir/libbmp/libbmp.c.obj"
+  "CMakeFiles/lab10.dir/libhttp/libhttp.c.obj"
+  "CMakeFiles/lab10.dir/omp_apps.c.obj"
+  "CMakeFiles/lab10.dir/server.c.obj"
+  "CMakeFiles/lab10.dir/server_utils.c.obj"
+  "CMakeFiles/lab10.dir/v_add.c.obj"
+  "lab10.exe"
+  "lab10.exe.manifest"
+  "lab10.pdb"
+  "liblab10.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
